c544453df5b8f725f51e86f0d7e06dcbf556e558
AristyoAgung <54440579+AristyoAgung@users.noreply.github.com>
2020-06-15 18:16:26 +0700

    Initial commit

---
+++ Test Suites/TesUjianPPL.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TesUjianPPL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>067e9503-011f-4749-ad3d-e9df7483f52e</testSuiteGuid>
+   <testCaseLink>
+      <guid>eadf07df-4eea-49ef-91d7-46fd78973ad9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Berhasil</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e447d49-59b7-4bb6-8f8e-c19e07c3c845</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Gagal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>85ad983e-59cc-4b94-9ee1-5612ee799b89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login if</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>304abace-0e68-45ca-8e37-59411ec197c3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/username</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>304abace-0e68-45ca-8e37-59411ec197c3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>1a2fe7d1-9b0a-4620-9eef-2cf06f1e85ec</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>998c7684-3b47-416c-915d-27bd6f10eb97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Insert Pasien</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d52848fc-6687-4b23-8e70-e0d0d3a8bbe1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Validasi data Pasien</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ac18222-a4ab-46f6-9a61-a9dde8d8806b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Insert Pasien Variabel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7ea139e0-e624-4a17-9173-ee45cd122aeb</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/input pasien</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7ea139e0-e624-4a17-9173-ee45cd122aeb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nama</value>
+         <variableId>eb12ff49-e899-48ff-b52c-83a6dce0ed8b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7ea139e0-e624-4a17-9173-ee45cd122aeb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nomorrekam</value>
+         <variableId>c5aba3d4-9c4b-48e2-bcb5-d683d7b17f96</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7ea139e0-e624-4a17-9173-ee45cd122aeb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nomorhp</value>
+         <variableId>59b39c8a-943a-4210-880a-c11292efb40c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7ea139e0-e624-4a17-9173-ee45cd122aeb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>alamat</value>
+         <variableId>bc8622c5-7e9f-4cb9-99c7-510c5bd36d57</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
